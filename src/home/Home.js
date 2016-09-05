95c63a0f36617dd87a361c61d118a152acd3d18d
sumanbh <sumanbh7@gmail.com>
2016-09-05 15:07:53 -0600

    add fixed navbar

--- src/home/Home.js
+++ src/home/Home.js
@@ -4,7 +4,7 @@ require('./home.component.scss');
 class Home extends Component {
     render() {
         return (
-            <div className="row">
+            <div className="row home-container">
               <div className="col-xs-offset-3 col-xs-9">
                 <div className="box">Welcome Page to be filled by Pax and Jesse</div>
               </div>
